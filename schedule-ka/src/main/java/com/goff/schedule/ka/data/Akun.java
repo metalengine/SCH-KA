75436f7922e398d35938001671af0d559a8b47c2
hastiie <hastie.audytra@gmail.com>
2014-05-20 00:28:00 +0700

    add data

---
+++ schedule-ka/src/main/java/com/goff/schedule/ka/data/Akun.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.goff.schedule.ka.data;
+
+/**
+ *
+ * @author ainx
+ */
+public class Akun {
+    private int id;
+    private String email;
+    private String password;
+    private String inisial;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getInisial() {
+        return inisial;
+    }
+
+    public void setInisial(String inisial) {
+        this.inisial = inisial;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
+}
